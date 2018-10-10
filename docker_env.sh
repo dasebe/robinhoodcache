11f637df8fcf5906bbd3eb0139e4d83c6826f25d
Benjamin Berg <bsberg@cs.cmu.edu>
2018-10-10 00:02:32 -0400

    adding compose yml

--- docker_env.sh
+++ docker_env.sh
@@ -2,3 +2,10 @@
 
 #the URL of the docker registry you will be using
 export REPO=
+
+# the URL of the data which will populate mysql backends
+export MYSQL_DATA_URL=
+
+
+#debugging variables, set to 1 to start the corresponding container in debug mode
+
